bcc6450fc586cfc8ca52579143349233c74f4aa8
uroskubet <uroskubet15@gmail.com>
2022-09-17 18:42:36 +0200

    trece

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Search from "../assets/svg/search";
+import { ConnectButton } from "web3uikit";
 const styles = {
   header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
   headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
@@ -34,14 +35,14 @@ export const Header = () => {
             <div className={styles.badge} />
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>Crypto Town</div>
+            <div className={styles.navLink}>Cryptown</div>
             <div className={styles.badge} />
           </div>
           <div className={styles.navItem}>
             <div className={styles.navLink}>Portfolio</div>
           </div>
           <div className={styles.navItem}>
-            <div className={styles.navLink}>Watch List</div>
+            <div className={styles.navLink}>Watchlist</div>
           </div>
           <div className={styles.navItem}>
             <div className={styles.navLink}>Products</div>
@@ -53,7 +54,7 @@ export const Header = () => {
         </nav>
 
         <div className="flex items-center">
-          {/* connect button */}
+          <ConnectButton />
           <div className={styles.inputContainer}>
             <Search />
             <input className={styles.input} placeholder="Search" />
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl="https://fwtevcs7vumh.usemoralis.com:2053/server"
+      appId="9OF6oYr4VwjXdTbyspCMtmleW0u3xud5pSRoNdQA"
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
