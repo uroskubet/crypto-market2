5cd073c86beebbcd55fa6e2a89caefa1be8c8082
uroskubet <uroskubet15@gmail.com>
2022-09-17 19:34:34 +0200

    trece

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { Header } from "../components/Header";
+import Trending from "../components/Trending";
 export default function Home() {
   return (
     <div className="min-h-screen ">
       <Header />
       <div className="mt-10" />
 
-      {/* Trending */}
+      <Trending />
 
       <div className="20" />
       {/* cmctable */}
